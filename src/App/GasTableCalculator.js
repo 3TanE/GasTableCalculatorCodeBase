516e4d6f816b8e0c61d72c10819ca8a8465ce876
jonathan schott-vaupel <jonathan.schott-vaupel@bitbucket.com>
2021-07-09 01:27:01 +0200

    initial commit

---
+++ src/App/GasTableCalculator.js
@@ -0,0 +1,161 @@
+import React, {useState} from 'react'
+import "./GasTableCalculator.scss"
+import {Button, FormControl, FormHelperText, MenuItem, Select, TextField} from "@material-ui/core";
+import 'katex/dist/katex.min.css';
+import {InlineMath, BlockMath} from 'react-katex';
+import inputFieldsConf from "./inputFieldsConf";
+import {subsonicColumns, supersonicColumns, VSWColumns} from "./ColumnDefinitions";
+import {DataGrid} from '@material-ui/data-grid';
+
+
+const GasTableCalculator = () => {
+    const [tableType, setTableType] = useState(0);
+    const [inputState, setInputState] = useState({})
+    const [calcListResults, setCalcListResults] = useState([])
+
+
+    const handleInputFieldChange = (event, id) => {
+        let origin = event.target.value;
+        let config = inputFieldsConf[tableType]
+        let keys = Object.keys(config);
+
+        let newState = {};
+        newState[id] = origin;
+        console.log(newState)
+        console.log(!config[id].sanityCheck(origin))
+        if (!config[id].sanityCheck(origin)) {
+            setInputState({...newState})
+            return;
+        }
+        let newMachNumber = config[id].toM(origin);
+        keys.map((key) => {
+            if (key != id) {
+                newState[key] = config[key].fromM(newMachNumber)
+            }
+        })
+        setInputState({...newState})
+    }
+
+    const transferResults = () => {
+        let keys = Object.keys(inputState)
+        if (keys.length > 0) {
+            if (calcListResults[tableType] === undefined) {
+                calcListResults[tableType] = [];
+            }
+            let temp = [...calcListResults[tableType]]
+            temp.push({id: calcListResults[tableType].length, ...inputState});
+            calcListResults[tableType] = temp;
+            setInputState({});
+
+        }
+
+
+    }
+    const getInputFields = () => {
+        let config = inputFieldsConf[tableType]
+        let keys = Object.keys(config);
+        return keys.map(key => {
+                let field = config[key]
+                let saintValue = field.sanityCheck(inputState[field.id] || "")
+                console.log(saintValue, inputState[field.id])
+                return (
+                    <TextField
+                        disabled={field.disabled || false}
+                        error={!saintValue}
+                        helperText={saintValue ? "" : <InlineMath>{field.error}</InlineMath>}
+                        key={field.id}
+                        id={field.id}
+                        label={<InlineMath>{field.label}</InlineMath>}
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        variant="outlined"
+                        value={inputState[field.id] || ""}
+                        onChange={(event) => {
+                            handleInputFieldChange(event, field.id)
+                        }}
+                    />)
+            }
+        )
+
+    }
+
+    return (
+        <div className={"gasTableCalculator"}>
+            <div className={"title"}>Gas Table Calculator</div>
+            <div className={"tableSelector"}>
+                <FormControl>
+
+                    <Select
+                        labelId="demo-simple-select-placeholder-label-label"
+                        id="demo-simple-select-placeholder-label"
+                        value={tableType}
+                        onChange={(event) => {
+                            setTableType(event.target.value)
+                        }}
+                        displayEmpty
+                    >
+                        <MenuItem value={0}>Subsonic Two atomic Gases</MenuItem>
+                        <MenuItem value={1}>Supersonic Two atomic Gases</MenuItem>
+                        <MenuItem value={2}>Vertical Shock Wave two atomic Gases </MenuItem>
+                    </Select>
+                    <FormHelperText>Select a Gas Table</FormHelperText>
+                </FormControl>
+            </div>
+            <div className={"inputs"}>
+                {getInputFields()}
+            </div>
+            <div className={"buttons"}>
+                <Button disabled={inputState.size === 0} onClick={transferResults}>Add to ResultLists</Button>
+            </div>
+            <div className={"resultContainer"}>
+                {calcListResults[0] ? <SubSonicResultList data={calcListResults[0]}/> : null}
+            </div>
+            <div>
+                {calcListResults[1] ? <SuperSonicResultList data={calcListResults[1]}/> : null}
+            </div>
+            <div>
+                {calcListResults[2] ? <VSWResultList data={calcListResults[2]}/> : null}
+            </div>
+
+        </div>
+    )
+}
+
+const SubSonicResultList = (props) => {
+    console.log(props.data)
+    return (
+        <div style={{height: 400, width: '100%'}}>
+            <div className={"title"}>Subsonic Calculations Results</div>
+            <DataGrid rows={props.data} columns={mapConfigToColumns(inputFieldsConf[0])} pageSize={5}/>
+        </div>)
+}
+const SuperSonicResultList = (props) => {
+    return (
+        <div style={{height: 400, width: '100%'}}>
+            <div className={"title"}>Supersonic Calculations Results</div>
+            <DataGrid rows={props.data} columns={mapConfigToColumns(inputFieldsConf[1])} pageSize={5}/>
+        </div>)
+
+}
+const VSWResultList = (props) => {
+    return (
+        <div style={{height: 400, width: '100%'}}>
+            <div className={"title"}>Vertical Shock Wave Calculations Results</div>
+            <DataGrid rows={props.data} columns={mapConfigToColumns(inputFieldsConf[2])} pageSize={5}/>
+        </div>)
+
+}
+
+const mapConfigToColumns = (config) => {
+    let keys = Object.keys(config);
+    return keys.map((key, i) => {
+        return {
+            field: config[key].id,
+            headerName: <InlineMath>{config[key].label}</InlineMath>,
+            width: config[key].width
+        }
+    });
+}
+export default GasTableCalculator
---
+++ src/App/inputFieldsConf.js
@@ -0,0 +1,94 @@
+const inputFieldsConf = [
+    {
+        machNumber: {
+            id: "machNumber",
+            label: "M",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0 && value <=1))return true;
+                return false;
+            },
+            error:"0 \\leq M \\leq 1",
+            toM:(m)=>(m),
+            fromM:(m)=>(m),
+            width:150
+        },
+        pToP0:{
+            id: "pToP0",
+            label: " \\frac{p}{p_0} ",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0.52828 && value <1))return true;
+                return false;
+            },
+            error:"0.52828 \\leq \\frac{p}{p_0} < 1",
+            toM:(x)=>((5**(0.5))*(x**(-2/7)-1)**0.5),
+            fromM:(m)=>((1+m**2/5)**(-3.5)),
+            width:150
+        },
+        rhoToRho0:{
+            id: "rhoToRho0",
+            label: " \\frac{\\rho}{\\rho_0} ",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0.63394 && value <1))return true;
+                return false;
+            },
+            error:"0.63394 \\leq \\frac{\\rho}{\\rho_0} < 1",
+            toM:(x)=>((5**(0.5))*(x**(-2/5)-1)**0.5),
+            fromM:(m)=>((1+m**2/5)**(-2.5)),
+            width:150
+        },
+        tToT0:{
+            id: "tToT0",
+            label: " \\frac{T}{T_0} ",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0.83333 && value <1))return true;
+                return false;
+            },
+            error:"0.83333 \\leq \\frac{T}{T_0} < 1",
+            toM:(x)=>(((5-5*x)**0.5)/(x)**0.5),
+            fromM:(m)=>(5/(5+(m**2))),
+            width:150
+        },
+        aToA0:{
+            id: "aToA0",
+            label: " \\frac{a}{a_0} ",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0.91287 && value <1))return true;
+                return false;
+            },
+            error:"0.91287 \\leq \\frac{a}{a_0} < 1",
+            toM:(x)=>(((5**(0.5))*(1-x*x)**0.5)/x),
+            fromM:(m)=>((1+m**2/5)**(-0.5)),
+            width:150
+        },
+        aStarToA0:{
+            id: "aStarToA0",
+            label: " \\frac{A^\\star}{A} ",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0 && value <=1))return true;
+                return false;
+            },
+            error:"0 \\leq \\frac{A^\\star}{A} \\leq 1",
+            disabled:true,
+            toM:(x)=>(x),
+            fromM:(m)=>(m*(6/(5+m*m))**3),
+            width:150
+        },
+        CP0:{
+            id: "CP0",
+            label: "Cp_0",
+            sanityCheck:(value)=>{
+                if(value==="" ||(value>=0 && value <=1.27561))return true;
+                return false;
+            },
+            error:"0 \\leq Cp_0 < 1",
+            disabled:true,
+            toM:(x)=>(x),
+            fromM:(m)=>((((1+(m*m)/5)**(3.5)-1)/(0.7*m*m))),
+            width:150
+        }
+    },
+    {},
+    {}
+]
+
+export default inputFieldsConf
